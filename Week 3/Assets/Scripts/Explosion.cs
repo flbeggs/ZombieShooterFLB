ea7d83fa47a7acae2491eaaf9cae0df339bdc208
FBeggs <freya.beggs@students.plymouth.ac.uk>
2018-03-23 08:48:07 +0000

    Work Added
    Added the rest of the completed work to the folder for repo

---
+++ Week 3/Assets/Scripts/Explosion.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+
+    public float destroyTime = 0.7f;
+
+    void Start()
+    {
+
+        Invoke("Die", destroyTime);
+
+    }
+
+    void Die()
+    {
+
+        Destroy(gameObject);
+
+    }
+}
